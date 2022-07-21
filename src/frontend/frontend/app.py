6b0978b9c6a7d7822baed6bcfbcb98ded7cddb6e
Scott Rogers <scott@scottandrewrogers.com>
2022-07-21 08:29:10 -0400

    updates

--- src/frontend/frontend/app.py
+++ src/frontend/frontend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import urllib.request
+import requests
 import os
 
 app = Flask(__name__)
@@ -10,22 +10,14 @@ def front_end():
     worldHost = os.environ.get('WORLD_HOST', "localhost")
     print("HELLO_HOST: " + helloHost)
     print("WORLD_HOST: " + worldHost)
-    resH = urllib.request.urlopen(urllib.request.Request(
-        url='http://' + helloHost + ':5001',
-        headers={'Accept': 'application/text'},
-        method='GET'), timeout=5)
 
-    resW = urllib.request.urlopen(urllib.request.Request(
-        url='http://' + worldHost + ':5001',
-        headers={'Accept': 'application/text'},
-        method='GET'), timeout=5)
+    resH = requests.get('http://' + helloHost + ':5001')
 
-    print(resH.status)
-    print(resH.reason)
-    resTextH = resH.read()
-    print(resTextH) 
-    print(type(resTextH))
-    return resTextH.decode("utf-8") + " " + resW.read().decode("utf-8") 
+    resW = requests.get('http://' + worldHost + ':5002')
+
+    print(resH.status_code)
+    print(resH.text)
+    return resH.text + ' ' + resW.text
 
 @app.route("/hash")
 def get_hash():
--- src/hello/hello/app.py
+++ src/hello/hello/app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    print("default route")
     return "Hello"
 
 @app.route("/hash")
