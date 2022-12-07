51cbf422d54574f73d7bc8ac28834aae3344ee1e
Scott Rogers <scrogatl@users.noreply.github.com>
2022-12-07 10:51:32 -0500

    updated

--- src/frontend/frontend/app.py
+++ src/frontend/frontend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import requests
 import os
+from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -9,20 +11,29 @@ def front_end():
     helloHost = os.environ.get('HELLO_HOST', "localhost")
     worldHost = os.environ.get('WORLD_HOST', "localhost")
     shard = os.environ.get('SHARD', "na")
-    print("HELLO_HOST: " + helloHost)
-    print("WORLD_HOST: " + worldHost)
-    print("SHARD: " + shard)
+    timeString = datetime.now().strftime("%H:%M:%S.%f")[:-3]
+    res = timeString + " - [frontend: " + shard + "] - "
+    try:
+        resH = requests.get('http://' + helloHost + ':5001')
+        if resH.status_code >= 300:
+            res += " hello status: " + str(resH.status_code) + " - " + resH.text + " - " + str(resH.headers)
+        else:
+            res += " hello status: " + str(resH.status_code) + " - " + resH.text 
 
+        resW = requests.get('http://' + worldHost + ':5002')
+        if resW.status_code >= 300:
+            res += " | world status: " + str(resW.status_code) + " - " + resW.text + " - " + str(resW.headers)
+        else:
+            res += " | world status: " + str(resW.status_code) + " - " + resW.text 
+    except Exception as e:
+        res += repr(e)
 
-    resH = requests.get('http://' + helloHost + ':5001')
-
-    resW = requests.get('http://' + worldHost + ':5002')
-
-    print("Hello status: " + str(resH.status_code))
-    print("Hello text: " + resH.text)
-    print("World status: " + str(resW.status_code))
-    print("World text: " + resW.text)
-    return "Frontend (" + shard + ") " + resH.text + ' ' + resW.text
+    print (res)
+        # print("Hello status: " + str(resH.status_code))
+        # print("Hello text: " + resH.text)
+        # print("World status: " + str(resW.status_code))
+        # print("World text: " + resW.text)
+    return res
 
 @app.route("/hash")
 def get_hash():
--- src/loadgen/loadgen/app.py
+++ src/loadgen/loadgen/app.py
@@ -8,17 +8,17 @@ import sys
 def loadgen():
     front_end = os.environ.get('FRONTEND_HOST', "localhost")
     print("FRONTEND_HOST: " + front_end)
-    
+    # 
     while (True):
         timeString = datetime.now().strftime("%H:%M:%S.%f")[:-3]
         try:
             res = requests.get('http://' + front_end + ':5000', timeout=.5)
             if res.status_code >= 300: 
-                print(timeString + " - Status: " + str(res.status_code) + " - " + res.text + " - " + str(res.headers))
+                print(timeString + " - [loadgen] - Status: " + str(res.status_code) + " - " + res.text + " - " + str(res.headers))
             else: 
-                print(timeString + " - Status: " + str(res.status_code) + " - " + res.text)
+                print(timeString + " - [loadgen] - Status: " + str(res.status_code) + " - " + res.text)
         except Exception as e:
-            print(timeString + " - Status: " + repr(e))
+            print(timeString + " - [loadgen] - Status: " + repr(e))
         time.sleep(1)
 
 if __name__ == '__main__':
