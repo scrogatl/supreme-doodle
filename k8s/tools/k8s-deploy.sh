ff7a66bd607ba56eaef517471c915d4c24e92626
scrogatl <scott@scottandrewrogers.com>
2025-07-24 09:04:45 -0400

    removed tools dir that used ebnsubst

--- k8s/tools/k8s-deploy.sh
+++
@@ -1,8 +0,0 @@
-#  envsubst < supreme-doodle/k8s/20-hello.yaml |  kubectl apply -f -
- for f in supreme-doodle/k8s/*; do
-    if [ -f "$f" ]; then
-        echo $f  
-        envsubst < $f | kubectl apply -f -
-    fi
-done
-
